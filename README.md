3a661ca92b68bd17a64f67f10f73de139715f537
Isael Carlos Bezerra <isaelc.bezerra@hotmail.com>
2022-11-03 13:46:28 -0300

    Atualizacoes-diversas

--- README.md
+++
@@ -1,1 +0,0 @@
-Gerador de CSS para Box-Shadow
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-class BoxShadowGenerated {
+class BoxShadowGenerator {
   constructor(
     horizontal,
     horizontalRef,
@@ -8,6 +8,11 @@ class BoxShadowGenerated {
     blurRef,
     spread,
     spreadRef,
+    color,
+    colorRef,
+    opacity,
+    opacityRef,
+    inset,
     previewBox,
     rule,
     webkitRule,
@@ -21,6 +26,12 @@ class BoxShadowGenerated {
     this.blurRef = blurRef;
     this.spread = spread;
     this.spreadRef = spreadRef;
+    this.color = color;
+    this.colorRef = colorRef;
+    this.opacity = opacity;
+    this.opacityRef = opacityRef;
+    this.inset = inset;
+    this.insetRef = inset.checked;
     this.previewBox = previewBox;
     this.rule = rule;
     this.webkitRule = webkitRule;
@@ -32,22 +43,14 @@ class BoxShadowGenerated {
     this.verticalRef.value = this.vertical.value;
     this.blurRef.value = this.blur.value;
     this.spreadRef.value = this.spread.value;
+    this.colorRef.value = this.color.value;
+    this.opacityRef.value = this.opacity.value;
 
     this.applyRule();
     this.showRule();
   }
 
-  applyRule() {
-    this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000`;
-    this.currentRule = this.previewBox.style.boxShadow;
-  }
-
-  showRule() {
-    this.rule.innerText = `box-shadow: ${this.currentRule}`;
-    this.webkitRule.innerText = `-webkit-box-shadow: ${this.currentRule}`;
-    this.mozRule.innerText = `-moz-box-shadow: ${this.currentRule}`;
-  }
-  update(type, value) {
+  updateValue(type, value) {
     switch (type) {
       case "horizontal":
         this.horizontalRef.value = value;
@@ -55,39 +58,79 @@ class BoxShadowGenerated {
       case "vertical":
         this.verticalRef.value = value;
         break;
+      case "spread":
+        this.spreadRef.value = value;
+        break;
       case "blur":
         this.blurRef.value = value;
         break;
-      case "spread":
-        this.spreadRef.value = value;
+      case "color":
+        this.colorRef.value = value;
+        break;
+      case "opacity":
+        this.opacityRef.value = value;
+        break;
+      case "inset":
+        this.insetRef = value;
         break;
     }
 
     this.applyRule();
     this.showRule();
   }
+
+  applyRule() {
+    const rgbValue = this.hexToRgb(this.colorRef.value);
+
+    const shadowRule = `${this.insetRef ? "inset" : ""} ${
+      this.horizontalRef.value
+    }px ${this.verticalRef.value}px ${this.blurRef.value}px ${
+      this.spreadRef.value
+    }px rgba(${rgbValue}, ${this.opacityRef.value})`;
+
+    this.previewBox.style.boxShadow = shadowRule;
+    this.currentRule = shadowRule;
+  }
+
+  showRule() {
+    const ruleWithSemiColon = `${this.currentRule};`;
+
+    this.rule.innerText = ruleWithSemiColon;
+    this.webkitRule.innerText = ruleWithSemiColon;
+    this.mozRule.innerText = ruleWithSemiColon;
+  }
+
+  hexToRgb(hex) {
+    return `${("0x" + hex[1] + hex[2]) | 0}, ${("0x" + hex[3] + hex[4]) | 0}, ${
+      ("0x" + hex[5] + hex[6]) | 0
+    }`;
+  }
 }
 
-//Seleção de elementos
+// Selecionar elementos
 const horizontal = document.querySelector("#horizontal");
 const horizontalRef = document.querySelector("#horizontal-value");
-
 const vertical = document.querySelector("#vertical");
 const verticalRef = document.querySelector("#vertical-value");
-
 const blur = document.querySelector("#blur");
 const blurRef = document.querySelector("#blur-value");
-
 const spread = document.querySelector("#spread");
 const spreadRef = document.querySelector("#spread-value");
-
 const previewBox = document.querySelector("#box");
 
-const rule = document.querySelector("#rule");
-const webkitRule = document.querySelector("#webkit-rule");
-const mozRule = document.querySelector("#moz-rule");
+const color = document.querySelector("#color");
+const colorRef = document.querySelector("#color-value");
 
-const boxShadow = new BoxShadowGenerated(
+const opacity = document.querySelector("#opacity");
+const opacityRef = document.querySelector("#opacity-value");
+
+const inset = document.querySelector("#inset");
+
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule span");
+const mozRule = document.querySelector("#moz-rule span");
+
+const boxShadow = new BoxShadowGenerator(
   horizontal,
   horizontalRef,
   vertical,
@@ -96,6 +139,11 @@ const boxShadow = new BoxShadowGenerated(
   blurRef,
   spread,
   spreadRef,
+  color,
+  colorRef,
+  opacity,
+  opacityRef,
+  inset,
   previewBox,
   rule,
   webkitRule,
@@ -104,23 +152,62 @@ const boxShadow = new BoxShadowGenerated(
 
 boxShadow.initialize();
 
-//Eventos
+// Eventos
 horizontal.addEventListener("input", (e) => {
   const value = e.target.value;
-  boxShadow.update("horizontal", value);
+
+  boxShadow.updateValue("horizontal", value);
 });
 
 vertical.addEventListener("input", (e) => {
   const value = e.target.value;
-  boxShadow.update("vertical", value);
+
+  boxShadow.updateValue("vertical", value);
 });
 
 blur.addEventListener("input", (e) => {
   const value = e.target.value;
-  boxShadow.update("blur", value);
+
+  boxShadow.updateValue("blur", value);
 });
 
 spread.addEventListener("input", (e) => {
   const value = e.target.value;
-  boxShadow.update("spread", value);
+
+  boxShadow.updateValue("spread", value);
+});
+
+color.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("color", value);
+});
+
+opacity.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("opacity", value);
+});
+
+inset.addEventListener("input", (e) => {
+  const value = e.target.checked;
+
+  boxShadow.updateValue("inset", value);
+});
+
+// Copiar regra
+const rulesArea = document.querySelector("#rules-area");
+const copyInstructions = document.querySelector("#copy-instructions");
+
+rulesArea.addEventListener("click", () => {
+  const rules = rulesArea.innerText.replace(/^\s*\n/gm, "");
+
+  navigator.clipboard.writeText(rules).then(() => {
+    copyInstructions.innerText = "Regra copiada com sucesso!";
+
+    setTimeout(() => {
+      copyInstructions.innerText =
+        "Clique no quadro acima para copiar as regras";
+    }, 1000);
+  });
 });
